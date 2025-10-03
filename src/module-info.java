15b13df4ec672e630764bffdcff60e2d41cb581f
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-10-03 13:08:06 +0600

    :bug:

--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-module FinalProject {
-    requires javafx.fxml;
-    requires javafx.controls;
-    requires javafx.graphics;
-    requires java.sql;
-    requires java.naming;
-    requires controlsfx;
-    opens sample;
-}
